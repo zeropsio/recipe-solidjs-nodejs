7cc8005e44789510d643c05382c97f5281daef38
Ales Rechtorik <foxxcz@gmail.com>
2024-06-24 21:06:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Zerops x Solid - Node.js
+
+Solid is a declarative Javascript library for creating fast, dynamic web apps with server-side rendering. [Zerops](https://zerops.io) makes deploying and running Solid.js apps, both server side rendered and static, a breeze. This recipe showcases the SSR version, see [zeropsio/recipe-solidjs-static](https://github.com/zeropsio/recipe-solidjs-static) for the static version.
+
+![solid](https://github.com/zeropsio/recipe-shared-assets/blob/main/covers/svg/cover-solid.svg)
+
+<br/>
+
+## Deploy to Zerops
+
+You can either click the deploy button to deploy directly on Zerops, or manually copy the [import yaml](https://github.com/zeropsio/recipe-solidjs-nodejs/blob/main/zerops-project-import.yml) to the import dialog in the Zerops app.
+
+[![Deploy on Zerops](https://github.com/zeropsio/recipe-shared-assets/blob/main/deploy-button/green/deploy-button.svg)](https://app.zerops.io/recipe/solid-nodejs)
+
+<br/>
+
+## Recipe features
+- Latest Solid version generated with [SolidStart](https://start.solidjs.com/) running on a load balanced **Zerops Node.js** service
+- Server Side Rendering (SSR) with Vinxi
+
+<br/>
+
+## Production vs. development
+This recipe is ready for production as is, and will scale horizontally by adding more containers in case of high traffic surges. If you want to achieve the highest baseline reliability and resiliace, start with at least two containers (add `minContainers: 2` in recipe YAML in the `app` service section, or change the minimum containers in "Automatic Scaling configuration" section of service detail).
+
+Additionally for you might want to consider setting up [Winston](https://github.com/winstonjs/winston) for advanced and structured logging.
+
+<br/>
+
+## Changes made over the default installation
+If you want to modify your existing Solid app to efficiently run on Zerops, there are no changes needed in the codebase on top of the standard installation, just add [zerops.yml](https://github.com/zeropsio/recipe-solidjs-nodejs/blob/main/zerops.yml) to your repository.
+
+<br/>
+<br/>
+
+Need help setting your project up? Join [Zerops Discord community](https://discord.com/invite/WDvCZ54).
---
+++ src/components/Steps.tsx
@@ -0,0 +1,67 @@
+import { Code } from "./Code";
+
+export function Steps() {
+const importyaml = `project:
+  name: recipe-solid
+  tags:
+    - zerops-recipe
+
+services:
+  - hostname: app
+    type: nodejs@20
+    enableSubdomainAccess: true
+    buildFromGit: https://github.com/zeropsio/recipe-solid-nodejs`.trim();
+  
+const zeropsyaml = `zerops:
+  - setup: app
+    build:
+      base: nodejs@20
+      buildCommands:
+        - pnpm i
+        - pnpm run build
+      deployFiles:
+        - .output
+        - node_modules
+        - package.json
+    run:
+      base: nodejs@20
+      ports:
+        - port: 3000
+          httpSupport: true
+      start: pnpm start`.trim();
+  return (
+    <div>
+      <div class="flex justify-center">
+        <p class="text-center mx-auto">Deploying will import the following structure (zerops-project-import.yml)<br/>
+        and use following (zerops.yml) instructions to build and deploy your app:</p>
+      </div>
+      <div class="grid grid-cols md:grid-cols-2 font-light gap-5 md:gap-10 pt-4">
+        <div class="flex flex-col gap-5">
+          <Code fileLink="https://github.com/zeropsio/recipe-solid-nodejs/blob/main/zerops-project-import.yml" file="zerops-project-import.yml" code={importyaml} />
+          <div
+          class="flex flex-col py-10 h-[260px] rounded-md gap-5 px-10 bg-[#F7F7F7]"
+        >
+          <a
+            href="https://github.com/zeropsio/recipe-solid-nodejs"
+            target="_blank"
+            class="primarybutton rounded-full text-center text-md duration-300 hover:no-underline"
+            >Recipe Source Code</a>
+          <a
+            href="https://discord.com/invite/WDvCZ54"
+            target="_blank"
+            class="discordbutton rounded-full text-center text-md duration-300 hover:no-underline"
+            >Discord</a>
+          <a
+            href="https://docs.zerops.io"
+            target="_blank"
+            class="zeropsbutton rounded-full text-center text-md duration-300 hover:no-underline"
+            >Documentation</a>
+        </div>
+        </div>
+        <div class="flex flex-col">
+          <Code fileLink="https://github.com/zeropsio/recipe-solid-nodejs/blob/main/zerops.yml" file="zerops.yml" code={zeropsyaml} />
+        </div>
+      </div>
+    </div>
+  );
+}
